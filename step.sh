f847e68f590571661d39bbdee836a361e7f4b621
Yaroslav <yaroslav@alterplay.com>
2019-11-26 17:11:14 +0200

    update

--- step.sh
+++ step.sh
@@ -1,55 +1,89 @@
 #!/bin/bash
-#
 
-# Check for optional params
 
-if [ -z "$add_bitrise_public_download_url" ]; then
-	add_bitrise_public_download_url=true
+custom_jira_field=""
+if [ -z "$jira_project_name" ]; then
+    echo "Jira Project Name is required."
+    usage
 fi
 
-# Check if this is merge commit
-
-if [[ $BITRISE_GIT_MESSAGE != *"Merge pull request"* ]]; then
-  echo "This commit it's not a merge commit"
-  exit -1
+if [ -z "$jira_url" ]; then
+    echo "Jira Url is required."
+    usage
 fi
 
-# Extract JIRA issue identifier from commit message
-
-jira_issue=`echo $BITRISE_GIT_MESSAGE | egrep -o '[A-Z]+-[0-9]+' | head -n 1`
-
-if [ ! "$jira_issue" ]; then
-	echo Commit message does not contain correct jira_issue
-	exit -1
+if [ -z "$jira_token" ]; then
+    echo "Jira token is required."
+    usage
 fi
 
-# Extract Pull Request identifier from commit message
-
-pull_request_id=`echo $BITRISE_GIT_MESSAGE | egrep -o '[0-9]+' | head -n 1`
-repository_url=`echo "$GIT_REPOSITORY_URL" | sed 's/.git//g' | tr : /`
-pull_request_url="https://$repository_url/pull/$pull_request_id"
-
-# Generate comment body
-
-comment_body="[Pull Request|$pull_request_url] for task $jira_issue was successfuly merged\nBuild number: $BITRISE_BUILD_NUMBER"
-if $add_bitrise_public_download_url = true; then 
-	comment_body="$comment_body\nDownload url: $BITRISE_PUBLIC_INSTALL_PAGE_URL"
+if [ -z "$from_status" ]; then
+    echo "Status of tasks for deployment is required."
+    usage
 fi
 
-comment_body="$comment_body\n$extra_info_in_comment"
+length=${#jira_project_name}
 
-echo $comment_body
-comment_url=$host/rest/api/2/issue/$jira_issue/comment
-echo "$comment_url"
-# add comment
-curl -D- -o /dev/null -u $user:$api_token -X POST -H "Content-Type: application/json" -d "{\"body\": \"$comment_body\"}" $comment_url
+cred="$jira_user:$jira_token"
 
-transition_url=$host/rest/api/2/issue/$jira_issue/transitions
-echo "$transition_url"
-# move to ready for qa
-res=$( curl -w %{http_code} -s --output /dev/null -D- -u $user:$api_token -X POST -H "Content-Type: application/json" -d "{\"transition\": {\"id\" : \"$qa_transition_id\"} }" $transition_url)
+token=`echo -n $cred | base64`
+echo "token $token"
+query=$(jq -n \
+    --arg jql "project = $jira_project_name AND status = '$from_status'" \
+    '{ jql: $jql, startAt: 0, maxResults: 20, fields: [ "id" ], fieldsByKeys: false }'
+);
 
-# if task was no_qa move directly to client
-if [ "$res" != "204" ]; then
-	curl -D- -o /dev/null -u $user:$api_token -X POST -H "Content-Type: application/json" -d "{\"transition\": {\"id\" : \"$no_qa_transition_id\"} }" $host/rest/api/2/issue/$jira_issue/transitions
-fi
+echo "Query to be executed in Jira: $query"
+
+tasks_to_close=$(curl -s \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Basic $token" \
+    --request POST \
+    --data "$query" \
+    "$jira_url/rest/api/2/search" | jq -r '.issues[].key'
+)
+
+echo "Tasks to transition: $tasks_to_close"
+
+for task in ${tasks_to_close}
+do
+            echo "Transitioning $task"
+            if [[ -n "$custom_jira_field" && -n "$custom_jira_field" ]]; then
+                echo "Setting version of $task to $custom_jira_field"
+                    query=$(jq -n \
+                        --arg version $custom_jira_field \
+                        "{ fields: { $custom_jira_field: [ \$version ] } }"
+                    );
+
+                curl \
+                    -H "Content-Type: application/json" \
+                    -H "Authorization: Basic $token" \
+                    --request PUT \
+                    --data "$query" \
+                    "$jira_url/rest/api/2/issue/$task"
+            fi
+
+                transition_id=$(curl -s \
+                    -H "Authorization: Basic $token" \
+                    "$jira_url/rest/api/2/issue/$task/transitions" |
+                    jq -r --arg t "$to_status"  '.transitions[] | select( .to.name == $t ) | .id'
+                )
+                echo "ids: $transition_id"
+                if [ -n "$transition_id" ]; then
+                    echo "Transitioning  $task to $to_status"
+                    query=$(jq -n \
+                        --arg ti $transition_id \
+                        '{ transition: { id: $ti } }'
+                    );
+                    echo "query: $query"
+                    curl \
+                        -H "Content-Type: application/json" \
+                        -H "Authorization: Basic $token" \
+                        --request POST \
+                        --data "$query" \
+                        "$jira_url/rest/api/2/issue/$task/transitions"
+                else
+                    echo "No matching transitions from status '$from_status' to '$to_status' for $task"
+                fi
+
+done
