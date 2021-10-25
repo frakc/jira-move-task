e437247be8b774b7b0d3d97bc65852a7cea19bf0
frakc <frakc@mail.ru>
2021-10-25 17:57:27 +0300

    add log

--- step.sh
+++ step.sh
@@ -24,7 +24,7 @@ length=${#jira_project_name}
 cred="$jira_user:$jira_token"
 
 token=`echo -n $cred | base64`
-echo jira_url
+echo "jira_url $jira_url"
 echo "label $token"
 query=$(jq -n \
 --arg jql "project = $jira_project_name AND status = '$from_status' AND text ~ $label_title_jira" \
